afbeeb73fb1bbb3e88200370a8bdfb8275f113a5
DnG17 <d.goxhufi@hs-mannheim.de>
2014-01-14 20:51:09 +0100

    änderung 20 uhr
    Home --> Startseite
    Impressum ---> Kontakt

--- Datenbankzugriff.php
+++ Datenbankzugriff.php
@@ -5,17 +5,6 @@ $dbUser = "d.goxhufi";
 $dbPw = "u2N_R7BrqW";
 $dbName = "d_goxhufi";
 
-//try
-
-//{
-    $db = new mysqli($dbHost,$dbUser,$dbPw,$dbName);
-/*}
-catch(PDOException $e){
-    echo "<p> Verbindung fehlgeschlagen!</p>";
-    echo '<p> .$e.</p>';
-    exit;
-}*/
-
-
+$db = new mysqli($dbHost,$dbUser,$dbPw,$dbName);
 
 ?>
--- website.php
+++ website.php
@@ -39,15 +39,15 @@ $manager->selectPage($id,$db);
 <div class="row">
     <div class="col-lg-4">
         <h2>1126139, Driton Goxhufi</h2>
-        <p><a class="btn btn-success" href="http://jonathan.sv.hs-mannheim.de/~d.goxhufi" role="button">Übungen;</a></p>
+        <p><a class="btn btn-success" href="http://jonathan.sv.hs-mannheim.de/~d.goxhufi" role="button">Übungen</a></p>
     </div>
     <div class="col-lg-4">
-        <h2>1234567, Saikam Alounlasy</h2>
-        <p><a class="btn btn-success" href="http://jonathan.sv.hs-mannheim.de/~s.alounlasy" role="button">Übungen;</a></p>
+        <h2>1132174, Saikam Alounlasy</h2>
+        <p><a class="btn btn-success" href="http://jonathan.sv.hs-mannheim.de/~s.alounlasy" role="button">Übungen</a></p>
     </div>
     <div class="col-lg-4">
         <h2>Code & Vorgehensweise</h2>
-        <p><a class="btn btn-info" href="" role="button">Code Details;</a></p>
+        <p><a class="btn btn-info" href="https://github.com/DnG17/Hochschule-Studium/commit/40e1a7e9d41fe4206225433054e60835f41d5cb9" role="button">Code Details</a></p>
     </div>
 </div>
 
